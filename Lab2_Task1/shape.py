366fea8bbdcb26560c08892b1471398204932646
ghaliaghazal <ghalia.ghazal2009@gmail.com>
2025-10-29 21:50:40 +0100

    uppdate

--- Lab2_Task1/shape.py
+++ Lab2_Task1/shape.py
@@ -4,4 +4,28 @@ class shape:
         self._x = x 
         self._y = y
 
+    @property
+    def x(self):
+        return self._x # läsbart x_värde
+
+    @property
+    def y(self):
+        return self._y # läsbart y_värde
+
+    
+    @property
+    def __str__(self):
+        return f"shape at ({self._x}, {self._y})"
+
+
+
+    @property
+    def __repr__(self):
+        return f"shape({self._x}, {self._y})"
+
+    @property
+    def move(self, dx, dy):
+        self._x += dx
+        self._y += dy
+
     
