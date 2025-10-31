65c6d9f47e7730d49a6667978a7593fbd102539b
ghaliaghazal <ghalia.ghazal2009@gmail.com>
2025-10-31 11:26:48 +0100

    uppdate

--- Lab2_Task1/geometry_circle.py
+++ Lab2_Task1/geometry_circle.py
@@ -1,9 +1,15 @@
 from shape import Shape
+from math import pi
 
 class Circle(Shape):
     def __init__ (self,x=0, y=0, radius=1):
         super().__init__(x, y)
         self.radius = radius
 
+    @property
+    def area(self):
+        return pi * (self.radius ** 2)
 
-
+    @property
+    def perimeter(self):
+        return 2 * pi * self.radius
